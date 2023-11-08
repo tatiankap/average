7978d6375f45523552f3ce72675cef4f4460e97c
tanyshkakoval28@gmail.com <tanyshkakoval28@gmail.com>
2023-11-08 07:42:06 +0200

    home work

---
+++ main.js
@@ -0,0 +1,5 @@
+const number1 = Number(prompt('Введіть перше число'))
+const number2 = Number(prompt('Введіть друге число'))
+const number3 = Number(prompt('Введіть третє число'))
+const avrg = (number1 + number2 + number3) / 3;
+alert(`середнє арифметичне цих чисел ${avrg}`)
